a0a55477183e7ea84f6d773420d30031fbf4b966
George Mavridis <u48gmv@dev-gmv.bgnet.de>
2019-06-12 13:35:36 +0200

    Update error handling create hnsc

--- SharePoint/CreateSiteCollectionInHostNamedSiteCollection.ps1
+++ SharePoint/CreateSiteCollectionInHostNamedSiteCollection.ps1
@@ -14,19 +14,24 @@ If ((Get-PSSnapIn -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyCon
 #$Template="BLANKINTERNET#0"; ### Publishing site
 #$Template="DEV#0"; ### Developer site
 #$Template="STS#0"; ### Standard team site
-#$Template="STS#3"; ### Modern team site
-$Template="SITEPAGEPUBLISHING#0"; ### Communication Site
+$Template="STS#3"; ### Modern team site
+#$Template="SITEPAGEPUBLISHING#0"; ### Communication Site
 $Language = 1031; ### German
 $WebApp = "http://sp2019vm";
 $rootSite = "http://portal.dev-gmv.bgnet.de"; ## This Host named site collection needs to exist. NOTE: No leading or trailing slashes
-$sitePath = "bgproj01"; ### The new site collection beneath the managed path "team. NOTE: No leading or trailing slashes"
+$sitePath = "bgint01"; ### The new site collection beneath the managed path "team. NOTE: No leading or trailing slashes"
 $siteManagedPath = "teamsites" ### The managed path to use. NOTE: No leading or trailing slashes ##
 $Owner = "dev-gmv\u48gmv";
-$Name ="BG Projekte 01";
-$Description ="Demo Projektseite";
+$Name ="BG Interessenvertreter 01";
+$Description ="Demo Interessenvertreter Seite";
 $ContentDB ="WSS_Content_096bc88ec8dc4b4585d30f903f45a63a";
 #$siteCollectionAdmins = @("ms\u48pru","ms\u48wst","ms\u48gmv","ms\u480tbs","ms\u48hhh","ms\u48mll","ms\u48gsc","ms\u48rqa","ms\u48lpd","ppro\sp48backupadm");
 $siteCollectionAdmins = @("dev-gmv\bguser");
+
+### Runtime variables ###
+$errorOccured = $true;
+$ErrorMessage = "";
+$FailedItem = ""; 
 
 
 ### Start logic for creating site collection  ###
@@ -49,33 +54,42 @@ Write-Host "Creating Host named site collection -> $site" -ForegroundColor Magen
 Try{    
     $newSpSite = New-SPSite $site  -HostHeaderWebApplication $WebApp -Name $Name -Description $Description  -OwnerAlias $Owner -language $Language -Template $Template -ContentDatabase $ContentDB -ErrorAction Stop;
     
-    <### Insert default permission groups ###>
+    <### Insert default permission groups
     $web=$newSpSite.RootWeb
     $web.AssociatedOwnerGroup = $null
     $web.AssociatedMemberGroup = $null
     $web.AssociatedVisitorGroup = $null
     $web.CreateDefaultAssociatedGroups($newSpSite.Site.Owner.UserLogin, $newSpSite.Site.SecondaryContact.UserLogin, $newSpSite.Title)
+     ###>
 
     Write-Host "Host named site collection created. Visit is on -> $site" -ForegroundColor Magenta -BackgroundColor Black;
+    $errorOccured = $false;
 }
 Catch{
     $ErrorMessage = $_.Exception.Message;
-    $FailedItem = $_.Exception.ItemName;        
+    $FailedItem = $_.Exception.ItemName;
+    $errorOccured = $true;     
+}
+
+
+### Only continue if no error occured ###
+if($errorOccured){
     Write-Error -Message $ErrorMessage;
-    break;     
+    Write-Host "Due to an error the script was halted" -ForegroundColor Red;
+}else{
+    ### Add additional site collection admins ###
+    foreach ($siteCollectionAdmin in $siteCollectionAdmins) 
+    {
+        $siteCollectionAdmin = New-SPUser -UserAlias $siteCollectionAdmin -Web $site -SiteCollectionAdmin;    
+        Write-Host "Following Site Collection Admin was added to the site -> " -ForegroundColor Green -BackgroundColor Black -NoNewline;
+        Write-Host $siteCollectionAdmin.DisplayName -ForegroundColor Yellow -BackgroundColor Black;
+
+    }
+
+    ### Enable/Disable feature ###
+    Disable-SPFeature -Identity "MDSFeature" -Url $site -Force -Confirm:$false -ErrorAction Ignore;  # Disable minimum download strategy feature
+    Write-Host "Following feature was disabled: " -ForegroundColor Green -NoNewline;
+    Write-Host "MDSFeature (Minimal Download stragety)" -ForegroundColor Yellow;
 }
 
-### Add additional site collection admins ###
-foreach ($siteCollectionAdmin in $siteCollectionAdmins) 
-{
-    $siteCollectionAdmin = New-SPUser -UserAlias $siteCollectionAdmin -Web $site -SiteCollectionAdmin;    
-    Write-Host "Following Site Collection Admin was added to the site -> " -ForegroundColor Green -BackgroundColor Black -NoNewline;
-    Write-Host $siteCollectionAdmin.DisplayName -ForegroundColor Yellow -BackgroundColor Black;
-
-}
-
-### Enable/Disable feature ###
-Disable-SPFeature -Identity "MDSFeature" -Url $site -Force -Confirm:$false -ErrorAction Ignore;  # Disable minimum download strategy feature
-Write-Host "Following feature was disabled: " -ForegroundColor Green -NoNewline;
-Write-Host "MDSFeature (Minimal Download stragety)" -ForegroundColor Yellow;
  
