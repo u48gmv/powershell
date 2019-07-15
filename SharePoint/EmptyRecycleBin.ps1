baa60d681f91492a8a3f4f2ffa8644b19e775d74
Mavridis George Michael <GeorgeMichael.Mavridis@bg-phoenics.de>
2019-07-15 13:24:45 +0200

    Adding empty recycle bin

---
+++ SharePoint/EmptyRecycleBin.ps1
@@ -0,0 +1,15 @@
+<# Load SharePoint PowerShell if not present #>
+If ( $null -eq  (Get-PSSnapIn -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue) )  
+{ Add-PSSnapIn -Name Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue }
+
+#Init params
+$url = "";
+
+#Get the Site Collection
+$Site = Get-SPSite $url;
+ 
+#Delete all from 1st Stage Recycle bin
+$Site.AllWebs | Foreach-object { $_.RecycleBin.MoveAllToSecondStage() };
+ 
+#Empty 2nd Stage Recycle bin
+$Site.RecycleBin.DeleteAll();
