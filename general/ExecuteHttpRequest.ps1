0371ea021ce290deb0e1bc5c17973f75b32ec447
George Michael Mavridis <GeorgeMichael.Mavridis@bg-phoenics.de>
2020-07-16 09:40:26 +0200

    Adding a http request example

---
+++ general/ExecuteHttpRequest.ps1
@@ -0,0 +1,10 @@
+﻿﻿Clear-Host
+$URL = "https://kntk.prod.notes-phas.ppro.bgnet.de/api/ADQuery?adOrgUnit=OU=BG-Phoenics,OU=Benutzer,DC=ms,DC=bgnet,DC=de&adFilter=(%26(objectCategory=person)(sAMAccountName=u*)(!sAMAccountName=u480*))&adAttributes=addFunction;company;companyExt;department;departmentNumber;Division;expertise;expertiseNumber;facsimileTelephoneNumber;floorLevel;functionNumber;givenName;infoExt;initials;ipPhone;l;mail;mailExt;mobile;otherDepartment;otherDepartmentNumber;otherDivision;otherExpertise;otherExpertiseNumber;otherFacsimileTelephone;otherFunctionNumber;otherL;otherPhone;otherPostalCode;otherStreetAddress;otherTeam;otherWorkFunction;postalCode;roomNumber;salutation;sAMAccountName;sn;specFunction;streetAddress;team;telephoneNumber;title;workFunction;memberOf";
+
+$strUser = 'ms\u48gmv';
+[System.Security.SecureString]$strPass = ConvertTo-SecureString -String "Admin-600" -AsPlainText -Force;
+$Cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList ($strUser, $strPass);
+
+$response = Invoke-Webrequest -uri $URL -credential $cred;
+$response.Content | Out-File -FilePath "\\bg48fs-mue10.ms.bgnet.de\batchverarbeitung\organigramm\memberdata.json";
+
