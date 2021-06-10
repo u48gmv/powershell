465f543ce916b487887b5a112bee66809bd3c2c0
George Michael Mavridis <GeorgeMichael.Mavridis@bg-phoenics.de>
2021-06-10 19:06:38 +0200

    Adding some new scipts

---
+++ SharePoint/Assign-alien-tenant-user.ps1
@@ -0,0 +1,43 @@
+﻿<#
+How to assign an alien tenant user
+#>
+Clear-Host
+
+Add-PSSnapin Microsoft.SharePoint.PowerShell
+
+$webApplicatioURL = Read-Host -Prompt "Provide the URL of the desired webapplication"
+
+$wa1 = Get-SPWebApplication $webApplicatioURL
+
+Write-Host "Clearing the PeoplePicker filter" -ForegroundColor Yellow
+
+$wa1.PeoplePickerSettings.SearchActiveDirectoryDomains.Clear()
+$wa1.Update()
+
+Write-Host "PeoplePicker filter cleared" -ForegroundColor Green
+
+$adsearchobj = New-Object Microsoft.SharePoint.Administration.SPPeoplePickerSearchActiveDirectoryDomain
+$dn = Read-Host -Prompt "Provide a domain name for the new filter. Example ms.bgnet.de"
+$adsearchobj.DomainName = $dn
+$sdn = Read-Host -Prompt "Provide the short domain name for the new filter. Example MS"
+$adsearchobj.ShortDomainName = $sdn
+$adsearchobj.IsForest = $true
+Write-Host "Setting new people picker filter to allow all domain users, to be selected." -ForegroundColor Yellow
+$wa1.PeoplePickerSettings.SearchActiveDirectoryDomains.Add($adsearchobj)
+$wa1.Update()
+Write-Host "Done resetting" -ForegroundColor Green
+Read-Host -Prompt "Now assign the alien user to the tenant. Once done hit ENTER or confirm to continue with the procedure"
+
+Write-Host "Re-clearing the PeoplePicker filter" -ForegroundColor Yellow
+$wa1.PeoplePickerSettings.SearchActiveDirectoryDomains.Clear()
+$wa1.Update()
+Write-Host "PeoplePicker filter cleared" -ForegroundColor Green
+
+Write-Host "Re-Setting new people picker filter to allow only tenant users." -ForegroundColor Yellow
+
+$tenantNumber = Read-Host -Prompt "Provide the tenant number. Example 48 for BG-Phoenics"
+$tenantGroup = ""
+$adsearchobj.CustomFilter = "(&(|((objectCategory=person)(objectClass=user))(objectClass=group))(|(mandantenNr=$($tenantNumber))(cn=UK$($tenantNumber)G-ORG-Alle))(!(userAccountControl:1.2.840.113556.1.4.803:=2)))"
+$wa1.PeoplePickerSettings.SearchActiveDirectoryDomains.Add($adsearchobj)
+$wa1.Update() 
+Write-Host "Done Re-Setting new people picker filter to allow only tenant users." -ForegroundColor Green
--- general/Get-PasswordExpiration-User.ps1
+++ general/Get-PasswordExpiration-User.ps1
@@ -1,4 +1,4 @@
-clear-host
+﻿clear-host
 
 
 #$accounts = Get-ADUser -filter {Enabled -eq $True -and PasswordNeverExpires -eq $False} –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" -SearchBase "OU=Laptop,OU=BG-Phoenics,OU=Benutzer,DC=ms,DC=bgnet,DC=de" -Server ms.bgnet.de | Sort-Object DisplayName
@@ -14,5 +14,22 @@ foreach($account in $accounts){
 
 
 
+-spurl="https://intra-ukn.ppro.bgnet.de/" -listname="kntkkontakte" -adou="OU=Migration,OU=UK-Nord,DC=ppro,DC=bgnet,DC=de" -filter="(&(objectCategory=person)(sAMAccountName=u*)(!sAMAccountName=u77oa*)(!sAMAccountName=u77es*)(!department=*extern*))" -type="full"
 
 
+
+Erfahrungsaustausch Gefahrstoffe
+TR AP-Ausbildung
+TR Arbeitsschutzorganisation AMS BAU
+TR Aus- und Fortbildung
+TR BEM
+TR BV Süd RLS
+TR Hochschulbeauftragte BG BAU
+TR Personal
+TR Personalentwicklung BG BAU
+TR Präv BK
+TR Präv Messtechnik
+TR Präv SuQ
+TR SAP-HCM
+TR Unfallauswertung
+TR-MIG-DEAKT
