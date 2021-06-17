bf2f5fd7af1e741a7baf2ff8703bfa29eae95e90
George Michael Mavridis <GeorgeMichael.Mavridis@bg-phoenics.de>
2021-06-17 11:41:03 +0200

    Adding ad user query

---
+++ general/Get-AD-Users-Based-On-Filter-Settings.ps1
@@ -0,0 +1,11 @@
+#$accounts = Get-ADUser -filter {userAccountControl -eq 514 -and objectCategory -eq "person" -and samAccountName -like "u48*"} –Properties "DisplayName", "msDS-UserPasswordExpiryTimeComputed" -SearchBase "DC=ms,DC=bgnet,DC=de" -Server ms.bgnet.de | Sort-Object DisplayName
+$accounts = Get-ADUser -filter {userAccountControl -eq 514 -and objectCategory -eq "person" -and samAccountName -like "u48*"} -Properties "samAccountName" -SearchBase "DC=ms,DC=bgnet,DC=de" -Server ms.bgnet.de | Sort-Object samAccountName
+write-host "Found $($accounts.Count) inactive account"
+<#
+foreach($account in $accounts){
+    write-host $account.samAccountName
+}
+#>
+
+Get-ADUser -filter {userAccountControl -eq 514 -and objectCategory -eq "person" -and samAccountName -like "u48*"} -Properties "samAccountName" -SearchBase "DC=ms,DC=bgnet,DC=de" -Server ms.bgnet.de | Sort-Object samAccountName | ForEach-Object -Process {Write-Host $_.samaccountname} | Out-File -FilePath .\test.txt
+Get-ADUser -filter {userAccountControl -eq 514 -and objectCategory -eq "person" -and samAccountName -like "u48*"} -Properties "samAccountName" -SearchBase "DC=ms,DC=bgnet,DC=de" -Server ms.bgnet.de | Sort-Object samAccountName | ForEach-Object -Process { $_.samaccountname | Out-File -Append -FilePath .\InactiveBGPhoenicsAccounts.txt}
