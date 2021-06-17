fe31d8ef5cee0ea5d311d7a495269485202f77b7
George Michael Mavridis <GeorgeMichael.Mavridis@bg-phoenics.de>
2021-06-17 12:24:13 +0200

    Adding Enabled attribute to get-aduser query

--- general/Get-AD-Users-Based-On-Filter-Settings.ps1
+++ general/Get-AD-Users-Based-On-Filter-Settings.ps1
@@ -9,3 +9,6 @@ foreach($account in $accounts){
 
 Get-ADUser -filter {userAccountControl -eq 514 -and objectCategory -eq "person" -and samAccountName -like "u48*"} -Properties "samAccountName" -SearchBase "DC=ms,DC=bgnet,DC=de" -Server ms.bgnet.de | Sort-Object samAccountName | ForEach-Object -Process {Write-Host $_.samaccountname} | Out-File -FilePath .\test.txt
 Get-ADUser -filter {userAccountControl -eq 514 -and objectCategory -eq "person" -and samAccountName -like "u48*"} -Properties "samAccountName" -SearchBase "DC=ms,DC=bgnet,DC=de" -Server ms.bgnet.de | Sort-Object samAccountName | ForEach-Object -Process { $_.samaccountname | Out-File -Append -FilePath .\InactiveBGPhoenicsAccounts.txt}
+
+Get-ADUser -filter {Enabled -eq $False} -Properties "samAccountName" -SearchBase "DC=ms,DC=bgnet,DC=de" -Server ms.bgnet.de | Sort-Object samAccountName | ForEach-Object -Process { $_.samaccountname | Out-File -Append -FilePath .\InactiveBGAllAccounts.txt}
+#Enabled -eq $True
